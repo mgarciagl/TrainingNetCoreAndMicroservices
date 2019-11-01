11982f677ba0819209649b39955f9a286157c973
mariano.garcia <mariano.garcia@EZE3-LLN-B02625>
2019-11-01 14:54:06 -0300

    reestructuring solution. adding a gateway web api. adding a project for unit tests.

---
+++ TrainingSuperville.Framework.UnitTest/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TrainingSuperville.Framework.UnitTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
